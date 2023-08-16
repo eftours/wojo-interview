bd440151612114a04c25b6b509f176ae9cdc13e7
Alex Hughes <alxhghs@gmail.com>
2023-08-16 09:30:26 -0700

    remove unused files

--- apps/api/src/db/room-configuration.model.ts
+++
@@ -1,7 +0,0 @@
-export type DbRoomConfiguration = {
-    capacity: number;
-    bedCode: string;
-    price: number;
-    name: string;
-    description: string;
-};
--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -2,16 +2,8 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
-import { RoomingList } from "./src/components/RoomingList";
-import { SelectTravelers } from "./src/components/SelectTravelers";
-
-// const getClient = (ip: string) => {
-//     return new ApolloClient({
-//         // uri: `${ip}:4000/graphql`,
-//         uri: `http://10.0.2.16:4000/graphql`,
-//         cache: new InMemoryCache(),
-//     });
-// };
+import { RoomingList } from "./src/app/rooming-list";
+import { SelectTravelers } from "./src/app/select-travelers";
 
 const client = new ApolloClient({
     uri: "http://localhost:4000/graphql",
@@ -20,22 +12,6 @@ const client = new ApolloClient({
 
 export default function Home() {
     const [nbTravelers, setNbTravelers] = useState(2);
-    // const [client, setClient] = useState<any>();
-
-    // useEffect(() => {
-    //     if (client) return;
-    //     const getIp = async () => {
-    //         const ip = await Network.getIpAddressAsync();
-    //         console.log({ ip }, Platform.OS);
-    //         setClient(getClient(ip));
-    //     };
-    //     getIp();
-    // });
-
-    // if (!client) {
-    //     return <Text>...loading</Text>;
-    // }
-
     return (
         <ApolloProvider client={client}>
             <View style={styles.container}>
